f12ad6e2715526db5afb5e12f48d3d90f2d1a347
samah-saidi <samah.saidi@polytechnicien.tn>
2025-12-06 23:37:32 +0200

    Complete EduPlatform full-stack application

--- EduPlatform/server/server.js
+++ EduPlatform/server/server.js
@@ -13,7 +13,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 app.use(express.json());
